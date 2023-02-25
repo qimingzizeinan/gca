07bf6aff79fe28236be48148a6fbcc7e053fabb4
qimingzizeinan <2399961615@qq.com>
2023-02-25 14:00:11 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ echo -e "---------docker Stop--------"
 docker stop my-project
 echo -e "---------docker Rm--------"
 # docker rm my-project
-docker rmi qimingzizeinan1/my-project
+docker rmi qimingzizeinan1/my-project:main
 echo -e "---------docker Pull--------"
-docker pull qimingzizeinan1/my-project
+docker pull qimingzizeinan1/my-project:main
 echo -e "---------docker Create and Start--------"
-docker run --rm -d -p 80:80 --name my-project qimingzizeinan1/my-project
+docker run --rm -d -p 80:80 --name my-project qimingzizeinan1/my-project:main
 echo -e "---------deploy Success--------"
